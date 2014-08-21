c582cdda72dffa3f655df22ee1871cdb855f2e93
vdua <varundua16@gmail.com>
2014-08-21 15:46:10 +0530

    adding readme for how to use and add samples document generator for samples and screenshot for the address lookup widget sample

---
+++ generateDoc.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+git checkout gh-pages 2>&1 1>/dev/null
+git merge --no-edit  -m "merging master with gh-pages branch" master 
+posts=_posts
+OPTS=`getopt -o ua --long update,all-sample: -n 'parse-options' -- "$@"`
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+while true; do
+        case "$1" in
+            -u | --update ) update=true; shift ;;
+            -a | --all-sample ) allsample=true shift ;;
+            -- ) shift; break ;;
+            * ) break ;;
+        esac
+done
+if [ -z $1 ] && [ -z $allsample ]; then
+    echo "Please provide the sample name to generate documentation or use -a flag to generate all samples";
+    exit;
+fi
+if [ -n $1 ]; then
+    sampleName=$1
+    #git checkout master -- $sampleName/docs 2>&1 1>/dev/null
+    #git reset HEAD . 2>&1 1>/dev/null
+    if [ ! -d $sampleName/docs ]; then
+        echo "The value provided is not a sample. Provide a sample name.";
+        exit;
+    fi
+    postName=`echo $sampleName`
+    echo "post name $postName"
+    post=`ls -al $posts | sed -n 's/..*\('"$postName"'\.markdown$\)/\1/p'`
+    if [ -n "$post" ] && [ -z "$update" ]; then
+        echo "documentation for $sampleName exists, use -u flag to update the documentation"
+        exit
+    fi
+    
+    ts=`date +%F`
+    filename="$posts/$ts-$postName.markdown"
+
+    touch $filename
+    screenshot=$sampleName/docs/screenshot.png
+    cp $screenshot img/screenshots/$postName.png
+    
+    echo --- > $filename
+    echo screenshot: /img/screenshots/$postName.png >> $filename
+    echo --- >> $filename
+
+    if [ ! -d samples ]; then
+        mkdir samples
+    fi
+    sampleinstructions=samples/$sampleName.markdown
+    touch $sampleinstructions
+    echo --- > $sampleinstructions
+    echo layout: sample >> $sampleinstructions
+    echo --- >> $sampleinstructions
+    cat $sampleName/README.md >> $sampleinstructions
+fi
